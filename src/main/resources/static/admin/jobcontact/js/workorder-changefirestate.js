5d27cc1006caf22b7ffd6b6a87f4a225272cc17d
HillSun <yuelian332@gmail.com>
2019-06-26 11:35:30 +0800

    功能完善

--- src/main/resources/static/admin/jobcontact/js/workorder-changefirestate.js
+++ src/main/resources/static/admin/jobcontact/js/workorder-changefirestate.js
@@ -5,7 +5,7 @@ function alreadywork(id) {
 	$.ajax({
 		cache : true,
 		type : "POST",
-		url : rootPath + "/OperatorController/alreadwork",
+		url : rootPath + "/OperatorController/alreadywork",
 		data : dataFormJson,
 		async : false,
 		error : function(request) {
@@ -15,20 +15,4 @@ function alreadywork(id) {
 			$.operate.saveSuccess(data);
 		}
 	});
-}
-function fire_setoff(id) {
-    var dataFormJson = {'deviceId':id}
-    $.ajax({
-        cache : true,
-        type : "POST",
-        url : rootPath + "/DeviceController/setfireoff",
-        data : dataFormJson,
-        async : false,
-        error : function(request) {
-            $.modal.alertError("系统错误");
-        },
-        success : function(data) {
-            $.operate.saveSuccess(data);
-        }
-    });
 }
